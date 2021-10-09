4c8654900726eedbadde89036b000613c089b71f
Divyakonapala <92133595+Divyakonapala@users.noreply.github.com>
2021-10-09 09:03:13 +0530

    Create swapping .c

---
+++ Swapping.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+main()
+{
+int a,b;
+printf("enter the first number\n");
+scanf("%d",&a);
+printf("enter the second number\n");
+scanf("%d",&b);
+printf("the numbers before swapping are:%d%d",a,b);
+printf("the numbers after swapping are:%d%d",b,a);
+return 0;
+}
